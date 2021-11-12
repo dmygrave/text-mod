df83589286e4ea436ff0e2e99411e0972e15abc3
dmygrave <sasnabl@rambler.ru>
2021-11-12 18:36:19 +0300

    bar

--- script_reg.js
+++ script_reg.js
@@ -3,11 +3,15 @@ let showPassword = document.querySelector('.show-password')
 let securityBar = document.querySelector('.security-bar')
 
 
-//Для того, чтобы при нажатии на чекбокс "Показать пароль- показывало пароль"
+//Для того, чтобы при нажатии на чекбокс "Показать пароль" - показывало пароль
 showPassword.onchange = function () {
     if (showPassword.checked) {
         password.type = 'text'
     } else {
         password.type = 'password'
     }
+}
+
+password.oninput = function () {
+     securityBar.style.width = password.value.length * 10 + '%'     //умножаем длину пароля на 10 , чтобы выводить полоску под строкой ввода пароля в зависимости от его длины для наглядности  
 }
